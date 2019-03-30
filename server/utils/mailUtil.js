993f0512a027b3514510c9f83d23c94a8e69180b
alon galperin <alonga90@gmail.com>
2019-03-30 12:10:08 +0300

    remove font from mail

--- server/utils/mailUtil.js
+++ server/utils/mailUtil.js
@@ -17,7 +17,7 @@ let mailSender = function(to, eventId, guestId) {
         to: to,
         subject: 'You are invited to an event!',
         html:  `<center>
-                <h2 style="font-family: Roboto">Hi, you are invited to an event! All the details in link:</h2> <br/> 
+                <h2>Hi, you are invited to an event! All the details in link:</h2> <br/> 
                 <a href="${url}guest.html?eventId=${eventId}&guestId=${guestId}">Come</a> <br/>
                 <img src="https://imgbbb.com/images/2019/03/30/alcohol-1281704_640.jpg">
                 </center>`
@@ -31,7 +31,7 @@ let mailSender = function(to, eventId, guestId) {
             to: to,
             subject: 'Your new event details',
             html:  `<center>
-                    <h2 style="font-family: Roboto">Congratulations! You created an event</h2> <h3> here is a link for managing:</h3> 
+                    <h2>Congratulations! You created an event</h2> <h3> here is a link for managing:</h3> 
                     <a href="${url}manage.html?manageId=${eventId}">Manage Event</a> <br/>
                     <img src="https://imgbbb.com/images/2019/03/30/people-2608316_640.jpg">
                     </center>`
