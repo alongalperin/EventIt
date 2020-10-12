09014e8ec5becde3f5fb5dcd68561736142ff8bd
alon galperin <alonga90@gmail.com>
2020-10-12 09:08:07 +0300

    Refactor init database function

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-let { sequelize } = require("./db/Sequelize");
+let { initDB } = require("./db/Sequelize");
 const routes = require('./routes');
 
 const port = process.env.PORT || 3000;
@@ -14,27 +14,6 @@ let app = express();
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "../public")));
 
-// initialize db. run only when  initDB() run bellow. usually the invoke line will be in comment
-let initDB = function () {
-  sequelize
-    .query("SET FOREIGN_KEY_CHECKS = 0")
-    .then(function () {
-      return sequelize.sync({
-        force: true,
-      });
-    })
-    .then(function () {
-      return sequelize.query("SET FOREIGN_KEY_CHECKS = 1");
-    })
-    .then(
-      function () {
-        console.log("Database synchronised.");
-      },
-      function (err) {
-        console.log(err);
-      }
-    );
-};
 // RESET THE DB
 // initDB();
 
