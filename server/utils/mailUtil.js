302df4148fdfc33d913dd96d48f92040930fa4e6
alon galperin <alonga90@gmail.com>
2020-10-21 12:17:19 +0300

    Change email image and reupload public folder

--- server/utils/mailUtil.js
+++ server/utils/mailUtil.js
@@ -38,7 +38,7 @@ let mailSenderService = function (to, eventId, guestId) {
       html: `<center>
                     <h2>Congratulations! You created an event</h2> <h3> here is a link for managing:</h3> 
                     <h3><a href="${url}manage/${eventId}">Manage Event</a></h3> <br/>
-                    <img style="width: 60vw; height: 80vh" src="https://github.com/alongalperin/EventIt/blob/master/public/img/email_image.jpg?raw=true" />
+                    <img style="width: 30vw; height: 40vh" src="https://github.com/alongalperin/EventIt/blob/master/client/src/img/email_image.jpg" />
                     </center>`,
     };
   }
